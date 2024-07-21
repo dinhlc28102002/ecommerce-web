bb97f2e42ab2cfc79d27a5052f7d9f82d1e7f0f7
Le Cong Dinh <hobao3210@gmail.com>
2024-07-21 17:29:11 +0700

    feat!: first commit display home and admin

---
+++ src/main/java/com/mosoftvn/MosoftvnApplication.java
@@ -0,0 +1,13 @@
+package com.mosoftvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosoftvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MosoftvnApplication.class, args);
+    }
+
+}
